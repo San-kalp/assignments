b899f4581d6f86e6c1241f835e05df73a6a137ce
San-kalp <sankalp.chordia20@vit.edu>
2024-01-03 05:08:21 +0530

    C

--- week-2/01-async-js/hard/3-promise-all.js
+++ week-2/01-async-js/hard/3-promise-all.js
@@ -5,19 +5,41 @@
  */
 
 function wait1(t) {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve()
+        },t)
+    })
 
 }
 
 function wait2(t) {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve()
+        },t)
+    })
 
 }
 
 function wait3(t) {
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve()
+        },t)
+    })
 
 }
 
-function calculateTime(t1, t2, t3) {
 
+
+function calculateTime(t1, t2, t3) {
+    const startTime = performance.now()
+    const promiseArr = [wait1(t1),wait2(t2),wait3(t3)]
+    return Promise.all(promiseArr).then(function(){
+        const endTime = performance.now()
+        return endTime - startTime
+    })
 }
 
 module.exports = calculateTime;
