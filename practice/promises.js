a2193fdf97c1e16140ced067060445997bb72182
San-kalp <sankalp.chordia20@vit.edu>
2024-01-02 12:50:23 +0530

    C

---
+++ practice/promises.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+
+// function sankalpReadFile(callback){
+//     fs.readFile("a.txt","utf-8",function(err,data){
+//         callback(data)
+//     })
+// }
+
+// function onDone(data){
+//     console.log(data)
+// }
+
+// sankalpReadFile(onDone)
+
+
+//Promises are syntactical sugars only 
+
+// function sankalpReadFile(){
+//     return new Promise(function(resolve){
+//         fs.readFile('a.txt','utf-8',function(err,data){
+//             resolve(data)
+//         })
+//     })
+// }
+
+// function onDone(data){
+//     console.log(data)
+// }
+
+// sankalpReadFile().then(onDone)
+
+
+function sankalpAsyncFunction(){
+    let p = new Promise(function(resolve){
+        resolve("Hi there")
+    })
+
+    return p
+}
+
+
+
+
+const value = sankalpAsyncFunction()
+value.then(function(data){
+    console.log(data)
+})
