551ead82b64d70e7e94c0980473aa1baa7c68695
San-kalp <sankalp.chordia20@vit.edu>
2024-01-03 13:27:13 +0530

    Get and post done

--- week-2/02-nodejs/todoServer-file.js
+++ week-2/02-nodejs/todoServer-file.js
@@ -19,6 +19,34 @@ app.get('/todos',function(req,res){
     })
 })
 
+app.post('/todos',function(req,res){
+    const id = uuidv4()
+    let newTodo = {
+        "id":id,
+        "title":req.body.title,
+        "descr":req.body.descr,
+    }
+
+    fs.readFile('todo.json','utf-8',function(err,data){
+        if(err){
+            res.status(500).send("Failed")
+        }
+        const todos = JSON.parse(data)
+        console.log(todos)
+        todos.push(newTodo)
+
+        fs.writeFile('todo.json',JSON.stringify(todos),'utf-8',function(err){
+            if (err){
+                res.status(500).send("Error")
+            }else{
+                res.send("Successfully written")
+            }
+        })
+
+    })
+
+})
+
 
 app.listen(port, function () {
     console.log(`Server listening on port ${port}`);
